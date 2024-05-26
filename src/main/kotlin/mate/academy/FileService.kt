3bf2545f3e2710ee7d4ade65551e40e934dead74
silchenko-arsen <silchenkoarsen@gmail.com>
2024-05-26 12:35:52 +0300

    add exercise

--- src/main/kotlin/mate/academy/FileService.kt
+++ src/main/kotlin/mate/academy/FileService.kt
@@ -1,11 +1,20 @@
 package mate.academy
 
-class FileService {
-    fun readFile(fileName: String): Result<String> {
+import java.io.File
+import java.io.IOException
+import kotlin.Result.Companion.failure
+import kotlin.Result.Companion.success
 
+class FileService {
+    fun readFile(fileName: String) = if (File(fileName).isFile) {
+        success(File(fileName).readText().uppercase())
+    } else {
+        failure(IOException("File does not exist or is not a valid file <$fileName>"))
     }
 
-    fun processFileContent(fileName: String): String {
-
+    fun processFileContent(fileName: String) = if (readFile(fileName).isSuccess) {
+        "Uppercase content:\n" + readFile(fileName).getOrThrow()
+    } else {
+        "Error: Cannot read file - <${readFile(fileName).exceptionOrNull()?.message}>"
     }
 }

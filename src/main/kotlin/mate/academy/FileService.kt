1fb1555f825882cc0bd166e80b327c857aedcc12
Nonzeq <bgdn.kobylchak@gmail.com>
2024-06-13 13:20:07 +0300

    added

--- src/main/kotlin/mate/academy/FileService.kt
+++ src/main/kotlin/mate/academy/FileService.kt
@@ -1,11 +1,26 @@
 package mate.academy
 
-class FileService {
-    fun readFile(fileName: String): Result<String> {
+import java.io.File
+import java.io.IOException
 
+class FileService {
+    companion object {
+        const val MSG = "Uppercase content:\n"
+    }
+    fun readFile(fileName: String): Result<String> {
+        val result = runCatching {
+            val file = File(fileName)
+            file.readText().uppercase()
+        }
+        return result
     }
 
     fun processFileContent(fileName: String): String {
-
+        try {
+            val file = File(fileName)
+            return MSG + file.readText().uppercase()
+        }catch (e: IOException) {
+            return "Error: Cannot read file - ${e.message}"
+        }
     }
 }

3b84e304294209ae201cb7d943d8d6dd76623bff
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-19 12:32:46 +0300

    feat [kotlin]: implement file reader & converter

--- src/main/kotlin/mate/academy/FileService.kt
+++ src/main/kotlin/mate/academy/FileService.kt
@@ -1,11 +1,18 @@
 package mate.academy
 
+import java.io.File
+import java.io.IOException
+
 class FileService {
-    fun readFile(fileName: String): Result<String> {
-
+    fun readFile(fileName: String): Result<String> = runCatching {
+        val file = File(fileName)
+        if (!file.exists()) {
+            throw IOException("File does not exist or is not a valid file $fileName")
+        }
+        file.readText().uppercase()
     }
 
-    fun processFileContent(fileName: String): String {
-
-    }
+    fun processFileContent(fileName: String): String = readFile(fileName)
+        .onSuccess { return "Uppercase content:\n$it" }
+        .getOrElse { "Error: Cannot read file - ${it.message}" }
 }

0dfe595450678456544ea2bac2a37c2fba919c53
Inna Diachkina <innadyachkina@gmail.com>
2024-09-15 07:44:14 +0300

    my solution

--- src/main/kotlin/mate/academy/FileService.kt
+++ src/main/kotlin/mate/academy/FileService.kt
@@ -1,11 +1,22 @@
 package mate.academy
 
+import java.io.File
+import java.io.IOException
+
 class FileService {
     fun readFile(fileName: String): Result<String> {
-
+        return runCatching {
+            val file = File(fileName)
+            file.readText().uppercase()
+        }
     }
 
     fun processFileContent(fileName: String): String {
-
+        val result = readFile(fileName)
+        return if (result.isSuccess) {
+            "Uppercase content:\n${result.getOrThrow()}"
+        } else {
+            "Error: Cannot read file - ${result.exceptionOrNull()?.message}"
+        }
     }
 }

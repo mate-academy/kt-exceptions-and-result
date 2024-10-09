47133c7815b816a3e7e7935deafa3a604685188b
Oleksand Kolisnyk <kolisnykol@gmail.com>
2024-10-09 23:44:12 +0300

    kom_solution

--- src/main/kotlin/mate/academy/FileService.kt
+++ src/main/kotlin/mate/academy/FileService.kt
@@ -1,11 +1,32 @@
 package mate.academy
 
+import java.io.File
+
 class FileService {
     fun readFile(fileName: String): Result<String> {
-
+        return runCatching {
+            File(fileName).readText().uppercase()
+        }
     }
 
-    fun processFileContent(fileName: String): String {
+//    @Throws(IOException::class)
+//    fun readFile(fileName: String): Result<String> {
+//        val file = File(fileName)
+//        val result = runCatching {
+//            file.readText().uppercase()
+//        }
+//        return result
+//    }
 
+    fun processFileContent(fileName: String): String {
+        val result = runCatching {
+            File(fileName).readText().uppercase()
+        }
+        return (
+            if (result.isSuccess)
+                "Uppercase content:\n" + result.getOrDefault("")
+            else
+                "Error: Cannot read file - $fileName"
+        )
     }
 }

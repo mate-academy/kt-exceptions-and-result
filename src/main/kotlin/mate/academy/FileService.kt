d05f9a2f3d39a4c99a6a5220c65b9c24fb6076cb
Danylo Kishchenko <amfich@gmail.com>
2024-11-22 01:42:01 +0200

    created logic to read text from a file, convert it to uppercase, and output the result.

--- src/main/kotlin/mate/academy/FileService.kt
+++ src/main/kotlin/mate/academy/FileService.kt
@@ -1,11 +1,25 @@
 package mate.academy
 
+import java.io.File
+import java.io.IOException
+
 class FileService {
     fun readFile(fileName: String): Result<String> {
-
+        return try {
+            val file = File(fileName)
+            require(file.exists() && file.isFile) {
+                "File does not exist or is not a valid file $fileName"
+            }
+            Result.success(file.readText().uppercase())
+        } catch (ex: IllegalArgumentException) {
+            Result.failure(IOException("File does not exist or is not a valid file $fileName", ex))
+        }
     }
 
     fun processFileContent(fileName: String): String {
-
+        return readFile(fileName).fold(
+            onSuccess = { content -> "Uppercase content:\n$content" },
+            onFailure = { ex -> "Error: Cannot read file - ${ex.message}" }
+        )
     }
 }

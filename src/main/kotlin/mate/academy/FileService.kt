22f9e904dc1f2e01665c01fba267748fc5eaab1e
Artem Klishch <artemklishch@gmail.com>
2024-11-28 15:12:57 +0200

    feat: files reading

--- src/main/kotlin/mate/academy/FileService.kt
+++ src/main/kotlin/mate/academy/FileService.kt
@@ -1,11 +1,21 @@
 package mate.academy
 
-class FileService {
-    fun readFile(fileName: String): Result<String> {
+import java.io.File
+import java.io.IOException
 
+class FileService {
+    @Throws(IOException::class)
+    fun readFile(fileName: String): Result<String> {
+        return runCatching {
+            File(fileName).readText().uppercase()
+        }
     }
 
     fun processFileContent(fileName: String): String {
-
+        return try {
+            "Uppercase content:\n${File(fileName).readText().uppercase()}"
+        } catch (exception: IOException) {
+            "Error: Cannot read file - ${exception.message}"
+        }
     }
 }

838557af65ea17c661a4ae06ca620d774b0bf5ff
Roman Voynahiy <romanvoynahiy@gmail.com>
2025-03-18 17:20:25 +0200

    Implement file reading and error handling with Result<T>.

--- src/main/kotlin/mate/academy/FileService.kt
+++ src/main/kotlin/mate/academy/FileService.kt
@@ -1,11 +1,24 @@
 package mate.academy
 
+import java.io.File
+import java.io.IOException
+
 class FileService {
     fun readFile(fileName: String): Result<String> {
-
+        val file = File(fileName)
+        return if(!file.exists() || !file.isFile) {
+            Result.failure(IOException("File does not exist or is not a valid file $fileName"))
+        } else {
+            Result.success(file.readText().uppercase())
+        }
     }
 
     fun processFileContent(fileName: String): String {
-
+        val result = readFile(fileName)
+        return if (result.isSuccess) {
+            "Uppercase content:\n${result.getOrNull()}"
+        } else {
+            "Error: Cannot read file - ${result.exceptionOrNull()?.message}"
+        }
     }
 }
